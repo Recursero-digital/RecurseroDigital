7e5e0e7c6cf38459187918d711e2d37bd14c4650
melissaBaran <melibaran4@gmail.com>
2025-09-16 20:13:31 -0300

    Agrego estructura base de front

---
+++ frontend/recursero-digital/src/App.jsx
@@ -0,0 +1,24 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Recursero digital</h1>
+      <p className="read-the-docs">
+      Una plataforma interactiva con juegos y recursos educativos para hacer el aprendizaje más dinámico, accesible y divertido. 🚀      </p>
+    </>
+  )
+}
+
+export default App
