dc4683d1dd832506ded48bb8d57f204c8576b94e
NSkyPare32 <nachoparejo95@gmail.com>
2025-09-22 23:35:28 -0300

    Resolver conflicto en App.jsx1

--- frontend/recursero-digital/src/App.jsx
+++ frontend/recursero-digital/src/App.jsx
@@ -1,12 +1,8 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
-<<<<<<< HEAD
-//import JuegoOrdenamiento from './components/games/JuegoOrdenamiento/JuegoOrdenamiento.jsx';
-=======
-// import JuegoOrdenamiento from './components/games/JuegoOrdenamiento/JuegoOrdenamiento.jsx';
->>>>>>> 9a82d1aa40a85364896dfdcdbd3e6870f1762827
-import JuegoEscritura from './components/games/JuegoEscritura/JuegoEscritura.jsx';
+import JuegoOrdenamiento from './components/games/JuegoOrdenamiento/JuegoOrdenamiento.jsx';
+//import JuegoEscritura from './components/games/JuegoEscritura/JuegoEscritura.jsx';
 
 /*function App() {
 
@@ -27,23 +23,23 @@ import JuegoEscritura from './components/games/JuegoEscritura/JuegoEscritura.jsx
   )
 }*/
 
-// function App() {
-
-//   return (
-//     <>
-//       <JuegoOrdenamiento />
-//     </>
-    
-//   );
-// }
 function App() {
 
   return (
     <>
-      <JuegoEscritura />
+      <JuegoOrdenamiento />
     </>
     
   );
 }
+// function App() {
+
+//   return (
+//     <>
+//       <JuegoEscritura />
+//     </>
+    
+//   );
+// }
 
 export default App
