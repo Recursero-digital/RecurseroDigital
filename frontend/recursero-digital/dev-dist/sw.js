96f6b85c4fea60cf6d7e928fa1bd7a82e7835a40
Matias <matiasgustavo1999@gmail.com>
2025-10-19 22:11:08 -0300

    fix layout docente

--- frontend/recursero-digital/dev-dist/sw.js
+++ frontend/recursero-digital/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a959eb95'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5l15kkrihqg"
+    "revision": "0.kdf58d50ea8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
