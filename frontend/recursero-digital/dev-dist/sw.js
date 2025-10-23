1e22279c4d2cea467a479b0ab3a1020a9ae585b2
Matias <matiasgustavo1999@gmail.com>
2025-10-22 23:51:41 -0300

    fix main

--- frontend/recursero-digital/dev-dist/sw.js
+++ frontend/recursero-digital/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a959eb95'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.43ob5fegj98"
+    "revision": "0.h8jsvi4mit"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
