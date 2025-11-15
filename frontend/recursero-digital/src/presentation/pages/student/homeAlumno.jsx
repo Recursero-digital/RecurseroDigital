deb13f04593967857e9f8206fa81587c70c0402d
NSkyPare32 <nachoparejo95@gmail.com>
2025-11-15 17:18:15 -0300

    Modicacion de diseño en HomaAlumno V1

--- frontend/recursero-digital/src/presentation/pages/student/homeAlumno.jsx
+++ frontend/recursero-digital/src/presentation/pages/student/homeAlumno.jsx
@@ -1,7 +1,9 @@
 import { useMemo } from "react";
+import { useNavigate } from "react-router-dom";
 import "../../styles/pages/homeAlumno.css";
 
 export default function HomeAlumno() {
+  const navigate = useNavigate();
 
   const userNameOrEmail = useMemo(() => {
     const storedEmail = localStorage.getItem("userEmail");
@@ -13,21 +15,28 @@ export default function HomeAlumno() {
     }
     return "ALUMNO";
   }, []);
+
+  const handleJugarClick = () => {
+    navigate('/alumno/juegos');
+  };
   return (
     <div className="main">
       
-      <section class="welcome-card">
+      <section className="welcome-card">
         <h1>¡Bienvenido {userNameOrEmail}!</h1>
         <p>
-          ¿Estás preparado para jugar y aprender con estos juegos de matemática?
+          ¡Bienvenido a Recursero Digital! Una plataforma educativa diseñada para hacer que el aprendizaje de las
+          matemáticas sea una experiencia divertida y emocionante.
         </p>
         <p>¡Descubre el mundo mágico de los números mientras te divertís!</p>
-      </section>
-      <section class="info-card">
+        
         <h2>¡Aprender matemáticas nunca fue tan divertido!</h2>
         <p>
-          Los juegos son para que ustedes aprendan habilidades matemáticas mientras se divierten. Cada juego
-          tiene 5 niveles y 3 actividades, numeros impresionantes y acumula puntos ¡A JUGAR!
+          Disfruten de estos juegos, son para que aprendan habilidades matemáticas mientras se divierten. Cada juego
+          tiene 5 niveles y 3 actividades, numeros impresionantes y acumula puntos{" "}
+          <button className="jugar-button" onClick={handleJugarClick}>
+            ¡A JUGAR!
+          </button>
         </p>
         <h3>
           ¡Comienza tu aventura en las matemáticas ¡Hoy mismo!
