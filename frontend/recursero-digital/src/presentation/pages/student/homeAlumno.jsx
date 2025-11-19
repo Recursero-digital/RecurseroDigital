818fcb44285e1d6a6c1cb3fce05e48df1e55e523
NSkyPare32 <nachoparejo95@gmail.com>
2025-11-19 18:52:35 -0300

    fix/dashboardAlumno-fuente

--- frontend/recursero-digital/src/presentation/pages/student/homeAlumno.jsx
+++ frontend/recursero-digital/src/presentation/pages/student/homeAlumno.jsx
@@ -16,23 +16,23 @@ export default function HomeAlumno() {
     <div className="main">
       
       <section className="welcome-card">
-        <h1>¡Bienvenido {userNameOrEmail}!</h1>
+        <h1>¡ Bienvenido {userNameOrEmail} !</h1>
         <p>
-          ¡Bienvenido a Recursero Digital! Una plataforma educativa diseñada para hacer que el aprendizaje de las
+          ¡ Bienvenido a Recursero Digital ! Una plataforma educativa diseñada para hacer que el aprendizaje de las
           matemáticas sea una experiencia divertida y emocionante.
         </p>
-        <p>¡Descubre el mundo mágico de los números mientras te divertís!</p>
+        <p>¡ Descubre el mundo mágico de los números mientras te divertís !</p>
         
-        <h2>¡Aprender matemáticas nunca fue tan divertido!</h2>
+        <h2>¡ Aprender matemáticas nunca fue tan divertido !</h2>
         <p>
           Disfruten de estos juegos, son para que aprendan habilidades matemáticas mientras se divierten. Cada juego
-          tiene 5 niveles y 3 actividades, numeros impresionantes y acumula puntos{" "}
+          tiene 3 niveles y 5 actividades, numeros impresionantes y acumula puntos{" "}
           <button className="jugar-button" onClick={handleJugarClick}>
-            ¡A JUGAR!
+            ¡ A JUGAR !
           </button>
         </p>
         <h3>
-          ¡Comienza tu aventura en las matemáticas ¡Hoy mismo!
+          ¡ Comienza tu aventura en las matemáticas ¡ Hoy mismo !
         </h3>
       </section>
     </div>
